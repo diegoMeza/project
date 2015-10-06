66d4f66668d0d5981bcf76dba50455326c963ac9
Carlos Andrés Charris S <darkklitos@gmail.com>
2015-10-06 18:53:58 -0500

    Test

--- 3/servidor2.js
+++ 3/servidor2.js
@@ -3,11 +3,23 @@
  */
 
 var servidor = require('http');
+var url = require('url');
 
-function arrancaServidor(requiere, respuesta){
-    console.log('Alguien se ha conectado');
-    respuesta.writeHead(200,{"Content-Type":"text/html"});
-    respuesta.write('<h1>El Servidor Funciona Correctamente</h1>');
-    respuesta.end();
+function iniciar(enrutar) {
+    function arrancaServidor(requiere, respuesta) {
+
+        var ruta = url.parse(requiere.url).pathname;
+        console.log('Alguien se ha conectado');
+
+        enrutar(ruta);
+
+
+        respuesta.writeHead(200, {"Content-Type": "text/html"});
+        respuesta.write('<h1>El Servidor Funciona Correctamente</h1>');
+        respuesta.end();
+    }
+
+    servidor.createServer(arrancaServidor).listen(8080);
 }
-servidor.createServer(arrancaServidor).listen(8080);
+
+exports.iniciar = iniciar;
